72bd8bfd8cea76d1ae5f456fe7f4e63fbb972439
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-15 10:13:50 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
+import { useState } from 'react';
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count, setcount]=useState(0)
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={()=>{
+          setcount(count+1);
+        }}>Clicked</button>
+        
     </div>
   )
 }
